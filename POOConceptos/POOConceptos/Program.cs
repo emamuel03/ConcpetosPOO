af9b162a34860df12752f2cef46cf40331227c66
Gomez Emmanuel <emmanuel.gomez.3@gmail.com>
2022-03-24 11:07:50 -0300

    Create Date Class

---
+++ POOConceptos/POOConceptos/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using POOConceptos;
+using System;
+
+Console.WriteLine("Hello World!");
+
+Date date1 = new Date(2022, 2, 11);
+
+try
+{
+    Console.WriteLine(new Date(2022, 2, 11));
+    Console.WriteLine(new Date(2022, 2, 11));
+    Console.WriteLine(new Date(2022, 2, 29));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
